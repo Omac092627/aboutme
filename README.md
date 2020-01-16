d4fc1b0062bb77f1e550d10751be864ca3a3e8e1
Omac092627 <oogunde0413@gmail.com>
2020-01-15 20:57:59 -0800

    created a draft about me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aboutme
+
+//This will be where I update what I have done on the project//
+
+//Today I'll be working on scaffolding and adding some basics to my about me//
---
+++ js/app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var welcomeMessage = prompt('Sup dude, what is your name?');
+alert('Welcome ' + welcomeMessage);
+
+
+
+var takeQuiz = prompt('Are you ready for the quiz?');
+
+if (takeQuiz.toLowerCase() === 'yes') {
+  alert('Aw yeah, lets do this!');
+
+} else if ('no') {
+  alert('Go back and study!');
+  end;
+
+} else {
+  alert('Something went wrong');
+}
+
+
+var firstQuestion = prompt('Is my name Lami Beach?');
+
+if (firstQuestion.toLowerCase() === 'yes') {
+  alert('Great job ' + welcomeMessage + ' That is my name!');
+
+} else if ('no') {
+  alert('Aww shucks, you don/t love me');
+
+} else {
+  alert('Something went wrong')
+}
+
+
+var secondQuestion = prompt('Did I attend Arizona State University')
+
+if (secondQuestion.toLowerCase() === 'yes') {
+  alert('You are on a roll!');
+
+} else if ('no') {
+  alert('Yikes, I thought you were my friend!');
+
+} else {
+  alert('Something went wrong');
+}
+
+
+var thirdQuestion = prompt('Was I an exotic Dancer?');
+
+if (thirdQuestion.toLowerCase() === 'yes') {
+  alert('I wish!');
+
+} else if ('no') {
+  alert('Correcto Mundo!');
+
+} else {
+  alert('Something went wrong');
+}
+
+
+var fourthQuestion = prompt('Have I sold a screenplay yet?');
+
+if (fourthQuestion.toLowerCase() === 'yes') {
+  alert('Unfortunately I have not');
+
+} else if ('no') {
+  alert('That is correct!');
+
+} else {
+  alert('Something went wrong');
+}
+
+
+var fifthQuestion = prompt('Do I like to party?')
+
+if (fifthQuestion.toLowerCase() === 'yes') {
+  alert('Yes, ' + welcomeMessage + ' lets party!')
+
+} else if ('no') {
+  alert('Sorry that is wrong')
+
+} else {
+  alert('Something went wrong')
+}
