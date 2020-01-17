5efe42325147ee8f3a775357d3e73af4ba5640d9
Omac092627 <oogunde0413@gmail.com>
2020-01-16 18:49:31 -0800

    final edits

--- js/app.js
+++ js/app.js
@@ -9,10 +9,12 @@ var takeQuiz = prompt('Are you ready for the quiz?');
 
 if (takeQuiz.toLowerCase() === 'yes') {
   alert('Aw yeah, lets do this!');
+  //console.log('Aw yeah, lets dow this!')
 
 } else if ('no') {
-  alert('You freaking suck dude');
+  alert('Try again');
   end;
+  //console.log('Try again')
 
 } else {
   alert('Something went wrong');
@@ -23,9 +25,10 @@ var firstQuestion = prompt('Is my name Lami Beach?');
 
 if (firstQuestion.toLowerCase() === 'yes') {
   alert('Great job ' + welcomeMessage + ' That is my name!');
-
+  //console.log('Great job ' + welcomeMessage + ' That is my name!')
 } else if ('no') {
   alert('Aww shucks, you don/t love me');
+  //console.log('Aww shucks, you don/t love me')
 
 } else {
   alert('Something went wrong')
@@ -36,9 +39,11 @@ var secondQuestion = prompt('Did I attend Arizona State University')
 
 if (secondQuestion.toLowerCase() === 'yes') {
   alert('You are on a roll!');
+  //console.log('You are on a roll')
 
 } else if ('no') {
   alert('Yikes, I thought you were my friend!');
+  //console.log('Yikes, I thought you were my friend!')
 
 } else {
   alert('Something went wrong');
@@ -49,9 +54,11 @@ var thirdQuestion = prompt('Was I an exotic Dancer?');
 
 if (thirdQuestion.toLowerCase() === 'yes') {
   alert('I wish!');
+  //console.log('I wish!')
 
 } else if ('no') {
   alert('Correcto Mundo!');
+  //console.log('Correcto Mundo!')
 
 } else {
   alert('Something went wrong');
@@ -62,9 +69,11 @@ var fourthQuestion = prompt('Have I sold a screenplay yet?');
 
 if (fourthQuestion.toLowerCase() === 'yes') {
   alert('Unfortunately I have not');
+  //console.log('Unfortunately I have not')
 
 } else if ('no') {
   alert('That is correct!');
+  //console.log('That is correct')
 
 } else {
   alert('Something went wrong');
@@ -75,9 +84,11 @@ var fifthQuestion = prompt('Do I like to party?')
 
 if (fifthQuestion.toLowerCase() === 'yes') {
   alert('Yes, ' + welcomeMessage + ' lets party!')
+  //console.log('Yes, ' + welcomeMessage + ' lets party!')
 
 } else if ('no') {
   alert('Sorry that is wrong')
+  //console.log('Sorry that is wrong')
 
 } else {
   alert('Something went wrong')
