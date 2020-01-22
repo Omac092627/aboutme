f91c4ecb268b4363151461c305c2ffa212ccc700
Omac092627 <oogunde0413@gmail.com>
2020-01-21 18:34:26 -0800

    added console logs

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var scoreNumber = 1;
 
 var welcomeMessage = prompt('Sup dude, what is your name?');
 alert('Welcome ' + welcomeMessage);
+console.log('Welcome');
 
 
 
@@ -13,15 +14,18 @@ var takeQuiz = prompt('Are you ready for the quiz?');
 
 if (takeQuiz.toLowerCase() === 'yes') {
   alert('Aw yeah, lets do this!');
-  //console.log('Aw yeah, lets dow this!')
+  console.log('Quiz is beginning');
 
-} else if ('no') {
-  alert('Try again');
+
+} else if (takeQuiz.toLowerCase() === 'no') {
+  alert('Go back and study');
+  console.log('They aren/t ready');
   end;
-  //console.log('Try again')
 
 } else {
   alert('Something went wrong');
+  console.log('something went wrong');
+
 }
 
 
@@ -30,13 +34,15 @@ var firstQuestion = prompt('Is my name Lami Beach?');
 if (firstQuestion.toLowerCase() === 'yes') {
   alert('Great job ' + welcomeMessage + ' That is my name!');
   alert(scoreNumber++);
-  //console.log('Great job ' + welcomeMessage + ' That is my name!')
+  console.log('Great job ' + welcomeMessage + ' That is my name!');
 } else if ('no') {
   alert('Aww shucks, you don/t love me');
-  //console.log('Aww shucks, you don/t love me')
+  console.log('wrong answer');
 
 } else {
   alert('Something went wrong')
+  console.log('Something went wrong');
+
 }
 
 
@@ -45,14 +51,16 @@ var secondQuestion = prompt('Did I attend Arizona State University');
 if (secondQuestion.toLowerCase() === 'yes') {
   alert('You are on a roll!');
   alert(scoreNumber++);
-  //console.log('You are on a roll')
+  console.log('You are on a roll');
 
 } else if ('no') {
   alert('Yikes, I thought you were my friend!');
-  //console.log('Yikes, I thought you were my friend!')
+  console.log('Yikes, I thought you were my friend!');
 
 } else {
   alert('Something went wrong');
+  console.log('Something went wrong');
+
 }
 
 
@@ -60,15 +68,16 @@ var thirdQuestion = prompt('Was I an exotic Dancer?');
 
 if (thirdQuestion.toLowerCase() === 'yes') {
   alert('I wish!');
-  //console.log('I wish!')
+  console.log('I wish!');
 
 } else if ('no') {
   alert('Correcto Mundo!');
   alert(scoreNumber++);
-  //console.log('Correcto Mundo!')
+  console.log('Correcto Mundo!');
 
 } else {
   alert('Something went wrong');
+  console.log('Something went wrong');
 }
 
 
@@ -76,15 +85,17 @@ var fourthQuestion = prompt('Have I sold a screenplay yet?');
 
 if (fourthQuestion.toLowerCase() === 'yes') {
   alert('Unfortunately I have not');
-  //console.log('Unfortunately I have not')
+  console.log('Unfortunately I have not');
 
 } else if ('no') {
   alert('That is correct!');
   alert(scoreNumber++);
-  //console.log('That is correct')
+  console.log('That is correct');
 
 } else {
   alert('Something went wrong');
+  console.log('Something went wrong');
+
 }
 
 
@@ -93,18 +104,19 @@ var fifthQuestion = prompt('Do I like to party?');
 if (fifthQuestion.toLowerCase() === 'yes') {
   alert('Yes, ' + welcomeMessage + ' lets party!')
   alert(scoreNumber++);
-  //console.log('Yes, ' + welcomeMessage + ' lets party!')
+  console.log('Yes, ' + welcomeMessage + ' lets party!');
 
 } else if ('no') {
   alert('Sorry that is wrong')
-  //console.log('Sorry that is wrong')
+  console.log('Sorry that is wrong');
 
 } else {
   alert('Something went wrong')
+  console.log('Something went wrong');
+
 }
 
 
-//console.log('sixthQuestion' + 'Guess a number between 1 and 10!');
 
 for (var i = 0; i < 4; i++) {
 
@@ -114,16 +126,18 @@ for (var i = 0; i < 4; i++) {
   if (sixthQuestion == 9) {
     alert('Good job, you read my mind! ' + welcomeMessage);
     alert(scoreNumber++);
+    console.log('good job');
     break;
   }
 
   else if (sixthQuestion < 9) {
     alert('higher')
+    console.log('higher');
   }
 
   else if (sixthQuestion > 9) {
     alert('lower');
-
+    console.log('lower');
   }
 
 }
@@ -140,30 +154,37 @@ for (var a = 0; a < 6; a++) {
   if (seventhQuestion === colors[0]) {
     alert('Yes, that is my favorite');
     alert(scoreNumber++);
+    console.log('correct answer');
     break;
 
   } else if (seventhQuestion === colors[1]) {
     alert('Yes, I love the color blue');
     alert(scoreNumber++);
+    console.log('correct answer');
     break;
 
   } else if (seventhQuestion === colors[2]) {
     alert('Oh yeah, green is the best');
     alert(scoreNumber++);
+    console.log('correct answer');
     break;
 
   } else if (seventhQuestion === colors[3]) {
     alert('Purple rhymes with Urkle, you are correct');
     alert(scoreNumber++);
+    console.log('correct answer');
     break;
   }
 
   else (seventhQuestion != colors)
   alert('Try again');
+  console.log('Try again');
 }
 
 
 alert('The correct answers were: red, green, blue, and purple');
+console.log('list of correct colors');
 
 
 alert('Final Score: ' + --scoreNumber);
+console.log('displays final score');
