85140d733dc057aa3c9455b5431ce5e343e1a722
ticochuck <chucklivilla@gmail.com>
2020-01-22 19:38:10 -0800

    copied js file. Final version

--- js/app.js
+++ js/app.js
@@ -33,6 +33,8 @@ if (takeQuiz.toLowerCase() === 'yes') {
 while (takeQuiz.toLowerCase() === 'yes') {
   console.log('My code only runs when the person is ready');
 
+  function firstQ() {
+
   var firstQuestion = prompt('Is my name Lami Beach?');
 
   if (firstQuestion.toLowerCase() === 'yes') {
@@ -48,8 +50,11 @@ while (takeQuiz.toLowerCase() === 'yes') {
     console.log('Something went wrong');
 
   }
+  
+  }
+firstQ();
 
-
+function secondQ() {
   var secondQuestion = prompt('Did I attend Arizona State University');
 
   if (secondQuestion.toLowerCase() === 'yes') {
@@ -66,8 +71,10 @@ while (takeQuiz.toLowerCase() === 'yes') {
     console.log('Something went wrong');
 
   }
+}
+secondQ();
 
-
+function thirdQ() {
   var thirdQuestion = prompt('Was I an exotic Dancer?');
 
   if (thirdQuestion.toLowerCase() === 'yes') {
@@ -83,8 +90,10 @@ while (takeQuiz.toLowerCase() === 'yes') {
     alert('Something went wrong');
     console.log('Something went wrong');
   }
+}
+thirdQ();
 
-
+function fourthQ() {
   var fourthQuestion = prompt('Have I sold a screenplay yet?');
 
   if (fourthQuestion.toLowerCase() === 'yes') {
@@ -101,8 +110,10 @@ while (takeQuiz.toLowerCase() === 'yes') {
     console.log('Something went wrong');
 
   }
+}
+fourthQ();
 
-
+function fifthQ() {
   var fifthQuestion = prompt('Do I like to party?');
 
   if (fifthQuestion.toLowerCase() === 'yes') {
@@ -119,9 +130,10 @@ while (takeQuiz.toLowerCase() === 'yes') {
     console.log('Something went wrong');
 
   }
+}
+fifthQ();
 
-
-
+function sixthQ() {
   for (var i = 0; i < 4; i++) {
 
     var sixthQuestion = prompt('Guess a number between 1 and 10!');
@@ -149,42 +161,27 @@ while (takeQuiz.toLowerCase() === 'yes') {
   if (sixthQuestion != 9) {
     alert('The answer is 9!');
   }
+}
+sixthQ();
+
+function seventhQ() {
 
   for (var a = 0; a < 6; a++) {
 
     var seventhQuestion = prompt('What is your favorite color?');
     var colors = ['red', 'blue', 'green', 'purple'];
 
-    if (seventhQuestion === colors[0]) {
+    if (seventhQuestion.toLowerCase() === colors[0] || seventhQuestion.toLowerCase() ===  colors[1] || seventhQuestion.toLowerCase() === colors[2] || seventhQuestion.toLowerCase() ===  colors[3]) {
       alert('Yes, that is my favorite');
       alert(scoreNumber++);
       console.log('correct answer');
       break;
-
-    } else if (seventhQuestion === colors[1]) {
-      alert('Yes, I love the color blue');
-      alert(scoreNumber++);
-      console.log('correct answer');
-      break;
-
-    } else if (seventhQuestion === colors[2]) {
-      alert('Oh yeah, green is the best');
-      alert(scoreNumber++);
-      console.log('correct answer');
-      break;
-
-    } else if (seventhQuestion === colors[3]) {
-      alert('Purple rhymes with Urkle, you are correct');
-      alert(scoreNumber++);
-      console.log('correct answer');
-      break;
-    }
-
-    else (seventhQuestion != colors)
-    alert('Try again');
+    }  else (seventhQuestion.toLowerCase() != colors)
+    alert('Try again'); 
     console.log('Try again');
   }
-
+}
+seventhQ();
 
 
   alert('The correct answers were: red, green, blue, and purple');
@@ -193,4 +190,5 @@ while (takeQuiz.toLowerCase() === 'yes') {
 
   alert('Final Score: ' + --scoreNumber);
   console.log('displays final score');
+  break;
 }
